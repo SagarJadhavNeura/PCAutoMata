dc0aac28b257f455ea052272b9d7fa96a6fb239e
SagarJadhavNeura <133340274+SagarJadhavNeura@users.noreply.github.com>
2024-02-16 16:02:27 +0100

    Update textToSpeech.py

--- textToSpeech.py
+++ textToSpeech.py
@@ -1,26 +1,13 @@
 # pip install pyttsx3
 import pyttsx3
 
-conv = pyttsx3.init()
 
-voices = conv.getProperty('voices')
+def speakTheText(textToBeSpoken): # to speak the text to be spoken
+    conv = pyttsx3.init()
+    conv.setProperty('voice', 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0') # For female voice
+    conv.setProperty('rate', 150) # voice speech rate (words per minute)
+    conv.say(textToBeSpoken)
+    conv.runAndWait()
 
-'''
-# Loop through voices to find a female voice
-for voice in voices:
-    print(voice)
-    if "Microsoft Zira Desktop - English (United States)" in voice.name:
-        conv.setProperty('voice', voice.id)
-        print(voice.id)
-        break
-'''
-conv.setProperty('voice', 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0') # For female voice
 
-conv.setProperty('rate', 130) # voice speech rate (words per minute)
-
-answer = input("Give any text.\n")
-
-conv.say(answer)
-conv.runAndWait()
-
-# wait 
+speakTheText("Hello, world!")

54efe43323bd09d4317c999963a41d54af08297a
SagarJadhavNeura <133340274+SagarJadhavNeura@users.noreply.github.com>
2024-02-16 15:37:19 +0100

    Update textToSpeech.py

--- textToSpeech.py
+++ textToSpeech.py
@@ -5,16 +5,22 @@ conv = pyttsx3.init()
 
 voices = conv.getProperty('voices')
 
+'''
 # Loop through voices to find a female voice
 for voice in voices:
-    #print(voice)
+    print(voice)
     if "Microsoft Zira Desktop - English (United States)" in voice.name:
         conv.setProperty('voice', voice.id)
+        print(voice.id)
         break
+'''
+conv.setProperty('voice', 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0') # For female voice
 
-conv.setProperty('rate', 150)
+conv.setProperty('rate', 130) # voice speech rate (words per minute)
 
 answer = input("Give any text.\n")
 
 conv.say(answer)
 conv.runAndWait()
+
+# wait 
